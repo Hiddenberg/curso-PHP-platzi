226ec7bd5bf3253ab94b47a00cdf639f37cdebb2
Victor Pazaran <victor_hals@hotmail.com>
2020-08-06 14:38:38 -0500

    Desplegando informacion de la base de datos desde en la pagina principal con exito

--- POO php/jobs.php
+++ POO php/jobs.php
@@ -3,44 +3,26 @@
 require_once 'vendor/autoload.php';
 
 /* a partir de php 7 podemos agrupar los use */
-use App\models\{Job, Project, Printable};
+use App\models\{Job, Project};
 
-/* ahora tenemos con los constructores podemos declarar el titulo y la descripcion al momento de crear el nuevo Job (objeto) */
-$job1 = new Job('PHP developer','Este trabajo es genial para PHP');
-$job1->visible = true;
-$job1->months = 13;
 
-$job2 = new Job('Python developer','Este trabajo es genial para python');
-$job2->visible = true;
-$job2->months = 16;
-
-$job3 = new Job('','Este trabajo es genial para sin titulo');
-$job3->setTitle('');
-$job3->description = 'Este trabajo es genial para sin titulo';
-$job3->visible = true;
-$job3->months = 23;
-
-/* NOTA: la forma de acceder a un metodo o atributo de un objeto en php es con la sintaxis $objeto->atrubuto */
+/* con esto creamos un array directamente con los registros que tengamos en la base de datos */
+$jobs = Job::all();
 
 $project1 = new Project('Project 1', 'Description');
-
-/* aqui declararemos un array de objetos en lugar de uno con cada job y sus propiedades */
-
-$jobs = [$job1,$job2,$job3];
-
 $projects = [$project1];
 
 
 
 /* con la interfaz printable nos aseguramos de que esto sea algo printable */
-function printElement(Printable $job) { /* AQUI USAMOS ESTA FUNCION PARA HACER QUE SI LOS TRABAJOS NO ESTAN SETEADOS PARA SER VISIBLES SE DETENGA LA EJECUCION DE ESTA FUNCION CON EL RETURN*/
-   if ($job->visible == false) {
+function printElement($job) { /* AQUI USAMOS ESTA FUNCION PARA HACER QUE SI LOS TRABAJOS NO ESTAN SETEADOS PARA SER VISIBLES SE DETENGA LA EJECUCION DE ESTA FUNCION CON EL RETURN*/
+/*    if ($job->visible == false) {
      return;
-   }
+   } */
    /* echo $idx; */
    echo '<li class="work-position">';
-   echo '<h5>' . $job->getTitle() . '</h5>';
-   echo '<p>' . $job->getDescription() . '</p>';
+   echo '<h5>' . $job->title . '</h5>';
+   echo '<p>' . $job->description . '</p>';
    echo '<p>' . $job->getDurationAsString() . '</p>';
    echo '<strong>Achievements:</strong>';
    echo '<ul>';
