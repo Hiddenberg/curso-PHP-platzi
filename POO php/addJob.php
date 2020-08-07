9b7c9d1d22dc69003dfc1abd42cae38798aa1bde
Victor Pazaran <victor_hals@hotmail.com>
2020-08-07 12:30:59 -0500

    implementando router basico para redirigir trafico desde un solo punto de entrada y evitar cargar el mismo codigo multiples veces

--- POO php/addJob.php
+++ POO php/addJob.php
@@ -1,29 +1,6 @@
 <?php
 
-require_once 'vendor\autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-use App\models\Job;
-use App\models\Project;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cursophp',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
+use App\Models\Job;
 
 if (!empty($_POST)){
    if ($_POST['job-type'] == 'job'){
--- POO php/index.php
+++ POO php/index.php
@@ -1,28 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
 use App\models\Job;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cursophp',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 
 include("jobs.php"); /* lo que hace es incluir otro archivo php desde el cual podemos agregar sus variables o usar sus funciones son de contexto global*/
 /* para esto tambien se pude hacer con la funcion require() que lo que hace es que detiene la ejecucion si el programa no existe */
