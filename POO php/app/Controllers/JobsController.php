e8e290bbe7bc77529403cd75398b8fc26186689c
Victor Pazaran <victor_hals@hotmail.com>
2020-08-11 21:03:15 -0500

    implementado validacion de datos y subida de archivos

--- POO php/app/Controllers/JobsController.php
+++ POO php/app/Controllers/JobsController.php
@@ -11,22 +11,33 @@ class JobsController extends BaseController {
       if ($request->getMethod() == 'POST'){
          
          $postData = $request->getParsedBody();
-         $jobValidator = v::key('title', v::stringType()->notEmpty())->key('description', v::stringType()->notEmpty());
+         $jobValidator = v::key('title', v::stringType()->notEmpty())
+         ->key('description', v::stringType()->notEmpty())
+         ->key('month', v::numericVal()->notEmpty()->max(99));
          
          try {
 
             $jobValidator->assert($postData);
 
+            $files = $request->getUploadedFiles();
+            $logo = $files['logo'];
+
+            if ($logo->getError() == UPLOAD_ERR_OK) {
+               $filename = $logo->getClientFilename();
+               $logo->moveTo("uploads/$filename");
+            }
             if ($postData['job-type'] == 'job'){      
                $job = new Job();
                $job->title = $postData['title'];
                $job->description = $postData['description'];
+               $job->months = $postData['month'];
                $job->save();
                echo "<script>alert('Trabajo registrado correctamente')</script>";
             } else if ($postData['job-type'] == 'project'){
                $project = new Project();
                $project->title = $postData['title'];
                $project->description = $postData['description'];
+               $project->months = $postData['month'];
                $project->save();
                echo "<script>alert('Proyecto registrado correctamente')</script>";
             }
